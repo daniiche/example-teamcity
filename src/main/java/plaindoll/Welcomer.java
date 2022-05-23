8289aa99958b2b942a47a1998852e7531d44ba4f
daniiche <47454130+daniiche@users.noreply.github.com>
2022-05-23 18:07:53 +0300

    add hunter method

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -7,4 +7,7 @@ public class Welcomer{
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
+	public String sayImAHunter() {
+		return "I am indeed, the skillest hunter in the world!.";
+	}
 }
